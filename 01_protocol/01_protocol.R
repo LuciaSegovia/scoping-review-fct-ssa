c83db9cebad1f3c628f583dd132d4f68b15a2659
LuciaSegovia <lsegoviarevilla@gmail.com>
2022-07-29 18:16:06 +0100

    tried to clean the protocol script

--- 01_protocol/01_protocol.R
+++ 01_protocol/01_protocol.R
@@ -1,18 +1,25 @@
 
+#Packages dependencies
+#install.packages("tidyverse")
+#install.packages("gt")
+
+
 library(tidyverse)
 library(gt)
 #library(gtExtras)
 
 
-#0) Loading the data
+# 1) Loading the data -----
 
 #food balance sheet (FAO, 2020)
 
 #loading path as downloaded from FAOSTAT
 
 supply <- paste0("FAOSTAT_data_", gsub("^0", "", format(Sys.Date(), "%m-%d-%Y")), ".csv")
+supply <- "FAOSTAT_data_4-25-2022.csv"
 
-read_csv(here::here("data", supply))
+fbs <- read_csv(here::here("data", supply))
+#fbs <- read_csv(here::here("data", "FBS_supply-regional-SSA_2014-2018.csv"))
 
 #regional FCDB: regional food composition data for SSA (Joy et al, 2014)
 #Concentration (100 g-1 fresh weight)
@@ -27,7 +34,8 @@ fct %>% select(3, 7) %>% distinct()
 
 #cleaning and preparing fct
 
-mn <- c("Energy", "Ca", "Cu", "Fe", "I", "Mg", "Se", "Zn")
+#mn <- c("Energy", "Ca", "Cu", "Fe", "I", "Mg", "Se", "Zn")
+mn <- c("Energy", "Ca",  "Fe", "I",  "Se", "Zn")
 
 fct <- fct %>% select(2:4, 8) %>%  filter(`...3` %in% mn) %>% 
   pivot_wider(
@@ -36,41 +44,18 @@ fct <- fct %>% select(2:4, 8) %>%  filter(`...3` %in% mn) %>%
   rename(region = "Supplementary Table 2. Food mineral composition data from literature sources, used in conjunction with Food Balance Sheets (FBSs) to estimate dietary mineral availability",
          food.name.original = "...4")
 
-##Getting countries and regions in Africa  
-#
-#africa <- raster::ccodes() %>%  filter(continent == "Africa")
-#
-##Fixing names in NAME_FAO to be the same as in food balance sheet
-#raster::ccodes() %>% filter(str_detect(NAME_FAO, "Congo"))
-#
-#africa$NAME_FAO[africa$NAME_FAO == "Cape Verde"] <-  "Cabo Verde"
-#africa$NAME_FAO[africa$NAME_FAO == "Congo, Republic of"] <-  "Congo"
-#africa$NAME_FAO[africa$NAME_FAO == "Congo, Dem Republic of"] <-  "Democratic Republic of the Congo"
-#africa$NAME_FAO[africa$NAME_FAO == "Swaziland"] <-  "Eswatini"
-#africa$NAME_FAO[africa$NAME_FAO == "Tanzania, United Rep of"] <-  "United Republic of Tanzania"
-#
-#1) Calculating MN supply in SSA
+
+#2) Calculating MN supply in SSA ----
 
 years <- c(2014:2018)
-#year < 2019
 
-#loading food supply in kg/cap/year to estimate MN supply
-qty <- read_csv(here::here("data", supply))%>%
+#Filtering food supply in kg/cap/year to estimate MN supply
+qty <- fbs %>%
   janitor::clean_names() %>% 
   filter(year %in% years, element_code != "664") #filter out years from after 2019 & supply in kcal 
                                                       #for reproducibility
 
 #Preparing food supply for merging w/ regional FCDB
-
-#1.1) Generating the variable Region per country 
-#checking country allocation per region
-
-# qty %>% 
-#   left_join(., africa %>% select(NAME_FAO, UNREGION1),
-#             by = c("area" = "NAME_FAO")) %>%
-#   filter(is.na(UNREGION1)) %>% pull(area)
-
-
 
 qty <- qty %>% 
   mutate(Region = case_when(
@@ -80,19 +65,13 @@ qty <- qty %>%
   str_detect(area, "Middle") ~ "M",
   TRUE ~ area))  
   
-#1.2) Unit conversion and mean food supply per food category and region (year, country)   
+# 2.2) Unit conversion and mean food supply per food category and region (year)   
 
 #Mean supply of the years 
 total_qty <- qty %>% group_by(item_code, item, area_code_fao, area, Region) %>% 
-  summarise(total_value = mean(value)*10/365) %>%  #mean food supply per country 
+  summarise(total_value = mean(value)*10/365) %>%  #mean food supply per region 
   ungroup() %>%  arrange(desc(total_value)) %>% 
   mutate_at("item", str_to_lower)
-
-#Total supply in each region (100g/capita/day)
-#total_qty <- mean_qty %>%  group_by(item_code, item, Region) %>% 
-#  summarise(total_value = sum(mean_value)*10/365) %>% 
-#  arrange(desc(total_value)) %>% 
-#  mutate_at("item", str_to_lower)
   
 #Binding food supply with fct by name and code
 fct_fao_codes <- fct %>% select(food.name.original) %>% distinct() %>% 
@@ -101,7 +80,8 @@ fct_fao_codes <- fct %>% select(food.name.original) %>% distinct() %>%
             by = c("food.name.original" = "item"))
 
 fct_fao_codes <- fct_fao_codes %>% 
-  mutate(food.name.original = ifelse(is.na(item_code), paste(food.name.original, "and products"), 
+  mutate(food.name.original = ifelse(is.na(item_code), 
+                                     paste(food.name.original, "and products"), 
                             food.name.original)) %>%
   select(-item_code) %>% 
   left_join(total_qty %>% select(item_code, item) %>% distinct(), 
@@ -154,19 +134,13 @@ fao_fct <- left_join(total_qty , fct, by = c("item_code",
   mutate_at(mn, as.numeric)
 
 #Food items NOT covered in the FCT  
-left_join(total_qty , fct, by = c("item_code", 
+(not_reported <- left_join(total_qty , fct, by = c("item_code", 
                                   "Region" = "region"), keep = TRUE) %>%
   filter(is.na(Energy)) %>% 
-  distinct(item)
+  group_by(item) %>% summarise(mean = mean(total_value, na.rm =T)))
 
 
-#NOT reported in the FCT
-# Miscellaneous
-# Infant food (below 0.1*100g/cap/day)
-# Alcohol, non-food -> remove bc is not food
-# Rape and Mustardseed (below 0.1*100g/cap/day)
-# Palm kernels -> remove because is < 0
-
+#Calculating energy and mineral supply per food item per region
 n <- length(fao_fct)
 
 for(i in 1:length(mn)){
@@ -180,7 +154,6 @@ colnames(fao_fct)[n+i] <- paste0(mn[i], "_cap_day")
 fao_fct %>% 
   ggplot(aes(Zn_cap_day, region)) + geom_boxplot()
 
-fao_fct %>% filter(Zn_cap_day >20)
 
 # 1.5) cleaning the dataset w/ only the needed variables
 
@@ -190,24 +163,24 @@ glimpse(fao_fct)
 
 top10 <-  fao_fct %>% dplyr::select(c(item_code.x:total_value, ends_with("day"))) %>% 
   pivot_longer(
-    cols = c(total_value:Zn_cap_day), 
+    cols = c(Energy_cap_day:Zn_cap_day), 
     names_to = "element", 
     values_to = "value"
   ) %>% rename(item_code = "item_code.x") %>% 
   group_by(element, item_code, item) %>% 
   summarise(ave = mean(value), 
             total = sum(value)) %>% 
-  ungroup() %>% 
-  group_by(element) %>% 
+  ungroup()  %>%   group_by(element) %>% 
   slice_max(ave, n= 10) 
 
 #list of foods
-food_list <- top10 %>% arrange(element) %>% distinct(item) %>% pull(item) %>% unique()
+food_list3 <- top10 %>% arrange(element) %>% distinct(item) %>% pull(item) %>% unique()
 ener_list1 <- top10 %>% filter(element == "Energy_cap_day") %>% arrange(item) %>%  pull(item) 
 
 #Table with the FAO food categories to be included
 
-top10 %>% ungroup() %>% select(1:4) %>% filter(element != "total_value") %>% 
+top10 %>% ungroup() %>% select(1:4) %>%
+  #filter(element != "total_value") %>% 
   pivot_wider(names_from = element, 
               values_from = ave) %>% 
   relocate(Energy_cap_day, .after = item) %>% arrange(desc(Energy_cap_day)) %>% 
@@ -227,6 +200,7 @@ ener_list2 <- ener %>% ungroup() %>%
   slice_max(ave, n = 10) %>%  distinct(item) %>% 
   arrange(item) %>% pull(item)
 
+setdiff(tolower(ener_list1), tolower(ener_list2))
 
 tolower(ener_list1) == tolower(ener_list2)
 
@@ -241,9 +215,12 @@ setdiff(tolower(ener_list2), food_list)
 
 #Table with the FAO food categories to be included
 
-elements <- c("Energy_cap_day", "Ca_cap_day" ,"Cu_cap_day"  ,"Fe_cap_day","I_cap_day","Mg_cap_day", "Se_cap_day", "Zn_cap_day" )
+#elements <- c("Energy_cap_day", "Ca_cap_day" ,"Cu_cap_day"  ,"Fe_cap_day","I_cap_day","Mg_cap_day", "Se_cap_day", "Zn_cap_day" )
+elements <- c("Energy_cap_day", "Ca_cap_day" ,
+                "Fe_cap_day","I_cap_day", "Se_cap_day", "Zn_cap_day" )
 
-
+c("Energy", "Ca" ,"Cu",
+  "Fe","I","Mg", "Se", "Zn" )
 
 top10 %>% ungroup() %>% select(1:4) %>% filter(element != "total_value") %>% 
   pivot_wider(names_from = element, 
@@ -257,11 +234,9 @@ top10 %>% ungroup() %>% select(1:4) %>% filter(element != "total_value") %>%
     columns = c("item_code", "item")) %>%
   tab_spanner(
     label = "Supply per capita per day",
-    columns = c("Energy", "Ca" ,"Cu",
-                "Fe","I","Mg", "Se", "Zn" ))  %>%
+    columns = mn)  %>%
   fmt_number(
-    columns = c("Energy", "Ca" ,"Cu",
-                "Fe","I","Mg", "Se", "Zn" ),
+    columns = mn ,
     decimals = 2,
     use_seps = TRUE) %>%
   fmt_missing(
@@ -276,10 +251,10 @@ top10 %>% ungroup() %>% select(1:4) %>% filter(element != "total_value") %>%
     item = "Desc.",
     Energy = html("Energy,<br>kcal"),
     Ca = html("Ca,<br>mg"),
-    Cu = html("Cu,<br>mg"),
+   # Cu = html("Cu,<br>mg"),
     Fe = html("Fe,<br>mg"),  
     I = html("I,<br>mcg"),
-    Mg = html("Mg,<br>mg"),
+  #  Mg = html("Mg,<br>mg"),
     Se = html("Se,<br>mcg"),
     Zn = html("Zn,<br>mg"))  %>%
   tab_options(
@@ -294,4 +269,3 @@ top10 %>% ungroup() %>% select(1:4) %>% filter(element != "total_value") %>%
   
   
   
-#write.csv(., here::here("output", "pre-selected-food-list-ssa.csv"), row.names = F)
